f1d919829a3a7266831eb16ceb42266f48b1ccb8
Hannah Hudson <hannah.hch@gmail.com>
2017-09-25 19:17:14 -0400

    finished header

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../styles/App.css';
 import QuestionCard from './QuestionCard.js';
 import Header from './Header.js';
@@ -9,9 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
           <Header/>
-        </div>
         <div className="App-list">
           <QuestionCard/>
           <QuestionCard/>
