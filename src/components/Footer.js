8a41247775e3e2e0426ae0f30f9856c17270171b
SamSteele01 <ssteele017@gmail.com>
2017-09-26 13:11:51 -0400

    Footer icon added.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import logo from "../styles/Screen-Shot.png";
 
 export default class Footer extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ export default class Footer extends Component {
   render() {
     return (
       <div className = 'footer-wrapper'>
+        <img src={logo} className="app-logo" alt="logo" />
         <h3>slackoverflow</h3>
       </div>
     );
