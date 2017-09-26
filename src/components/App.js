8a4017b89277c28dfa166c808576d25833090c35
Hannah Hudson <hannah.hch@gmail.com>
2017-09-26 12:26:07 -0400

    added binds

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import QuestionCard from './QuestionCard.js';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import Jumbotron from "./Jumbotron.js";
 import QuestionCards from './QuestionCards.js';
+import QuestionPageApp from './QuestionPageApp.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.handleIncomingData = this.handleIncomingData.bind(this);
+    this.state ={
+      questionID: false
+    };
+  }
+
+  handleIncomingData(value){
+    this.setState({ questionID: value });
+
+  }
+
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header sendDataUpToParent={this.handleIncomingData}/>
         <Jumbotron/>
         <div className="question-cards-wrapper">
-          <QuestionCards/>
-          <QuestionCard/>
-          <QuestionCard/>
-          <QuestionCard/>
-
+        {this.state.questionID ? (<QuestionPageApp invariable={this.state.questionID} sendDataUpToParent={this.handleIncomingData}/>) : (<QuestionCards sendDataUp={this.handleIncomingData}/>)}
         </div>
-        <div className="App-Footer">
-          <Footer/>
-        </div>
+        <Footer/>
       </div>
     );
   }
