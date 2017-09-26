bb56efdb0fd1117a17719a0eda17c34a87c9cad5
Hannah Hudson <hannah.hch@gmail.com>
2017-09-26 18:32:23 -0400

    answer

--- src/components/AnswerCard.js
+++ src/components/AnswerCard.js
@@ -8,15 +8,13 @@ export default class AnswerCard extends Component {
     return(
       <div>
         <div className = "answer-title-header">
-          <div className = "answer-header-top">
             <div className = "answer-header-left">
               <h2>answer title goes here</h2>
             </div>
-            <div className = "answer-header-right">
-              
-            </div>
           </div>
-        </div>
+          <div className = "answer-body-text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
 
 
 
