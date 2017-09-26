6bc742c2efc108f4dad2ff4a60f775f5d8b06100
Hannah Hudson <hannah.hch@gmail.com>
2017-09-26 18:14:30 -0400

    working on answers

---
+++ src/components/AnswerCard.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+export default class AnswerCard extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+        <div className = "answer-title-header">
+          <div className = "answer-header-top">
+            <div className = "answer-header-left">
+              <h2>answer title goes here</h2>
+            </div>
+            <div className = "answer-header-right">
+              
+            </div>
+          </div>
+        </div>
+
+
+
+
+      </div>
+    )
+  }
+}
