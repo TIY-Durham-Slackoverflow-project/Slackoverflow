39a7ff8157c441809a65d68752ef121e7965265d
Hannah Hudson <hannah.hch@gmail.com>
2017-09-28 10:17:42 -0400

    added styles

--- src/components/App.js
+++ src/components/App.js
@@ -31,9 +31,9 @@ class App extends Component {
     this.state ={
       token: null,
       displayForm: false,
-      questionID: "false",
+      questionID: false,
       profilePage: false,
-      postQuestion: false
+      postQuestion: "false"
 
     };
   }
@@ -66,7 +66,7 @@ class App extends Component {
 
   navigateBackRequest(){
     this.setState({
-      questionID: false, 
+      questionID: false,
       postQuestion: false
     });
   }
